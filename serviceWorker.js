1632578b3016004fb151ea37449bd4b1bd058136
nhanifah <45675831+aliffirdi@users.noreply.github.com>
2023-02-14 01:41:33 +0700

    optimize

---
+++ serviceWorker.js
@@ -0,0 +1,45 @@
+let cacheNames = 'pages-cache-v1';
+const urlsToCache = [
+    '/',
+    '/index.html',
+];
+
+self.addEventListener('install', function (event) {
+    // Perform install steps
+    event.waitUntil(
+        caches.open(cacheNames)
+            .then(function (cache) {
+                console.log('Opened cache');
+                return cache.addAll(urlsToCache);
+            })
+    );
+    self.skipWaiting();
+});
+
+self.addEventListener('fetch', function (event) {
+    event.respondWith(caches.match(event.request)
+        .then(function (response) {
+            if (response) {
+                return response;
+            }
+            return fetch(event.request);
+        })
+    );
+});
+
+self.addEventListener('activate', function (event) {
+    let cacheWhitelist = ['pages-cache-v1'];
+    event.waitUntil(
+        caches.keys().then(function (cacheNames) {
+            return Promise.all(
+                cacheNames.map(function (cacheName) {
+                    if (cacheWhitelist.indexOf(cacheName) === -1) {
+                        return caches.delete(cacheName);
+                    }
+                })
+            );
+        })
+    );
+});
+
+
